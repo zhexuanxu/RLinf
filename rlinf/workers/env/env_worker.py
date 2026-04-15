06e80cbf1f0aa74bf7c8eae806d48b8c1c28da03
xzxuan <xzx0307@mail.ustc.edu.cn>
2026-04-15 07:31:57 +0000

    fix: fix the bug that num_traj may bigger than the true value

--- rlinf/workers/env/env_worker.py
+++ rlinf/workers/env/env_worker.py
@@ -474,7 +474,7 @@ class EnvWorker(Worker):
         current_dones = chunk_dones[:, -1]  # [num_envs] bool
         prev = self.eval_prev_done[stage_id]
         newly_done = current_dones & ~prev.to(current_dones.device)
-        self.eval_prev_done[stage_id] = current_dones.clone()
+        self.eval_prev_done[stage_id] = prev | current_dones
 
         if newly_done.any():
             if "final_info" in infos:
